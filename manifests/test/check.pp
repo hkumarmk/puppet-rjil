fec7a9fa959725e7ec20b014861859be3f4eb58c
hkumarmk <hkumarmk@gmail.com>
2015-04-09 15:48:24 +0530

    test::check should support validation check also

--- manifests/test/check.pp
+++ manifests/test/check.pp
@@ -2,17 +2,25 @@
 # Class: rjil::test::cinder
 #   Adding tests for cinder services
 #
+# [*test_consumer*]
 
 define rjil::test::check(
-  $port    = 0,
-  $address = '127.0.0.1',
-  $ssl     = false,
-  $type    = 'http',
+  $port     = 0,
+  $address  = '127.0.0.1',
+  $ssl      = false,
+  $type     = 'http',
+  $consumer = 'consul_service',
 ) {
 
   include rjil::test::base
 
-  file { "/usr/lib/jiocloud/tests/service_checks/${name}.sh":
+  if $consumer == 'consul_service' {
+    $file_path = "/usr/lib/jiocloud/tests/service_checks/${name}.sh"
+  } elsif $consumer == 'validation' {
+    $file_path = "/usr/lib/jiocloud/tests/${name}.sh"
+  }
+
+  file { $file_path:
     content => template("rjil/tests/${type}_check.sh.erb"),
     owner   => 'root',
     group   => 'root',
