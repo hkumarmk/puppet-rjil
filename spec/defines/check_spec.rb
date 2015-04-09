39630ba98efd48050b6ea00027e0696b107bad45
hkumarmk <hkumarmk@gmail.com>
2015-04-09 15:48:24 +0530

    test::check should support validation check also

--- spec/defines/check_spec.rb
+++ spec/defines/check_spec.rb
@@ -14,7 +14,7 @@ describe 'rjil::test::check' do
 
   context 'with http running on 8080' do
     let :params do
-      { 
+      {
         :name    => 'fooservice',
         :port    => 8080,
         :address => '127.0.0.1',
@@ -22,7 +22,7 @@ describe 'rjil::test::check' do
         :type    => 'http',
       }
     end
- 
+
     it do
       should contain_class('rjil::test::base')
       should contain_file('/usr/lib/jiocloud/tests/service_checks/fooservice.sh') \
@@ -33,4 +33,25 @@ describe 'rjil::test::check' do
     end
   end
 
+  context 'with https running on 443, with validation' do
+    let :params do
+      {
+        :name     => 'fooservice',
+        :port     => 443,
+        :address  => '127.0.0.1',
+        :ssl      => false,
+        :type     => 'http',
+        :consumer => 'validation',
+      }
+    end
+
+    it do
+      should contain_class('rjil::test::base')
+      should contain_file('/usr/lib/jiocloud/tests/fooservice.sh') \
+        .with_content(/check_http -H 127.0.0.1 -p 443/) \
+        .with_owner('root') \
+        .with_group('root') \
+        .with_mode('0755')
+    end
+  end
 end
