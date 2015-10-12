336554b679060b9f6ae6c1617f8203d14b76e890
hkumarmk <hkumarmk@gmail.com>
2015-10-12 06:44:46 +0530

    Make system_nonsystem_disks fact work even no /dev/disk/by-path
    Without any non-system disks, the operating system may not create directory
    /dev/disk/by-path, so this fact should work in that case also.

--- lib/facter/system_nonsystem_disks.rb
+++ lib/facter/system_nonsystem_disks.rb
@@ -1,21 +1,30 @@
 if Facter.value(:kernel) == 'Linux'
   disk_by_path_directory = '/dev/disk/by-path/'
-  nonsystem_disks = []
-  Dir.entries(disk_by_path_directory).reject{|d| d =~ /-part/}.each do |disk|
-    next unless File.blockdev?(disk_by_path_directory + disk)
-    nonsystem_disks << File.basename(File.realpath(disk_by_path_directory + disk))
-  end
-
-  system_disks = Facter.value(:blockdevices).split(/,/) - nonsystem_disks
-  unless nonsystem_disks.empty?
-    Facter.add(:nonsystem_disks) do
-      setcode { nonsystem_disks.sort.join(',') }
+  if File.directory?(disk_by_path_directory)
+    nonsystem_disks = []
+    Dir.entries(disk_by_path_directory).reject{|d| d =~ /-part/}.each do |disk|
+      next unless File.blockdev?(disk_by_path_directory + disk)
+      nonsystem_disks << File.basename(File.realpath(disk_by_path_directory + disk))
     end
-  end
 
-  unless system_disks.empty?
+    system_disks = Facter.value(:blockdevices).split(/,/) - nonsystem_disks
+    unless nonsystem_disks.empty?
+      Facter.add(:nonsystem_disks) do
+        setcode { nonsystem_disks.sort.join(',') }
+      end
+    end
+
+    unless system_disks.empty?
+      Facter.add(:system_disks) do
+        setcode { system_disks.sort.join(',') }
+      end
+    end
+  else
     Facter.add(:system_disks) do
-      setcode { system_disks.sort.join(',') }
+      setcode { Facter.value(:blockdevices) }
+    end
+    Facter.add(:nonsystem_disks) do
+      setcode { '' }
     end
   end
 end
